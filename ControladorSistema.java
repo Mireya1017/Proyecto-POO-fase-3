f6cfd20fa5b01e32e8a38f179573ffc692effca3
PabloA-FuentesB <fue25844@uvg.edu.gt>
2025-09-08 23:02:33 -0600

    Atributos ControladorSistema.java

--- ControladorSistema.java
+++ ControladorSistema.java
@@ -1,3 +1,8 @@
-class ControladorSistema {
-    
-}
+import java.util.*;
+
+/*
+Controlador
+ */
+public class ControladorSistema {
+    private List<Usuario> usuariosRegistrados = new ArrayList<>();
+    private Usuario usuarioActual;
