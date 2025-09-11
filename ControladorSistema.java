20e10242eef473ca13bffff1e60a1ed8621a9876
PabloA-FuentesB <fue25844@uvg.edu.gt>
2025-09-10 23:51:17 -0600

    Update ControladorSistema.java
    Registro de usuario y Inicio de Sesion.

--- ControladorSistema.java
+++ ControladorSistema.java
@@ -6,3 +6,29 @@ Controlador
 public class ControladorSistema {
     private List<Usuario> usuariosRegistrados = new ArrayList<>();
     private Usuario usuarioActual;
+    
+public void registrarUsuario(Usuario usuario) {
+        if (usuario == null) return;
+        boolean existe = false;
+        for (Usuario u : usuariosRegistrados) {
+            if (u.getCorreo().equalsIgnoreCase(usuario.getCorreo())) {
+                existe = true;
+            }
+        }
+        if (!existe) {
+            usuariosRegistrados.add(usuario);
+            usuario.registrarse();
+        } else {
+            System.out.println("El correo ya está registrado.");
+        }
+    }
+
+public boolean iniciarSesion(String correo, String contraseña) {
+    for (Usuario u : usuariosRegistrados) {
+        if (u.autenticarse(correo, contraseña)) {
+            usuarioActual = u;
+            return true;
+            }
+        }
+        return false;
+    }
